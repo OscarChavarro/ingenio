b072b750e36fc4757ba00fda3942238497537bcf
SithMasterBDSM <jedilink@gmail.com>
2015-12-24 21:47:16 -0500

    0h10min: Script de ejecución condicional considera el host para seleccionar modo de uso

--- runWithExternalMongo.sh
+++ runWithExternalMongo.sh
@@ -1,4 +1,13 @@
 export HOSTNAME=`hostname`
-export ROOT_URL=http://ingenio.cubestudio.co
-export MONGO_URL=mongodb://ingenio.cubestudio.co:27017/ingenio
-meteor --production --port 80 run
+
+if [ $HOSTNAME == "ingenio.cubestudio.co" ]; then
+    echo RUNNING APPLICATION IN PRODUCTION MODE
+    export ROOT_URL=http://ingenio.cubestudio.co
+    export MONGO_URL=mongodb://ingenio.cubestudio.co:27017/ingenio
+    meteor --production --port 80 run
+else
+    echo RUNNING APPLICATION IN DEVELOPMENT MODE
+    export ROOT_URL=http://localhost:3000
+    export MONGO_URL=mongodb://ingenio.cubestudio.co:27017/ingenio
+    meteor --port 3000 run
+fi
