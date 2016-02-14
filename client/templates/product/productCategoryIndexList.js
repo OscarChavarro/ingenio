00093873f8b7e1e4f8c7561bb0294e083ae0b413
Oscar Chavarro <jedilink@gmail.com>
2016-02-14 02:19:21 +0000

    0h05min: Cambio de estructura en el menú para usar listas

--- client/templates/product/productCategoryIndexList.js
+++ client/templates/product/productCategoryIndexList.js
@@ -5,7 +5,7 @@ Template.productCategoryIndexList.helpers({
 });
 
 Template.productCategoryIndexList.events({
-     // Desactivado por solicitud de Alexis
+    // Desactivado por solicitud de Alexis
     /*
     "click .productCategoryMenuOption": function (event, template) {
         event.preventDefault();
@@ -26,4 +26,30 @@ Template.productCategoryIndexList.events({
         });
     }
     */
+});
+
+Template.productCategoryIndexList.helpers({
+    dbProductByCategoryId: function(parent) {
+        var arr = [
+            {name: "Producto 1", url: "http://www.google.com"},
+            {name: "Producto 2", url: "http://www.youtube.com"}
+        ];
+        /*
+        var catId = parent._id;
+        Meteor.call("getSubcategoriesByCategoryId", catId, function (error, children) {
+            if (!valid(error) && valid(children)) {
+                var i;
+                console.log("Tengo hijos: " + children.length);
+                for ( i in children ) {
+                    console.log("  - " + children[i]);
+                }
+            }
+            else {
+                var arr = [{name: "No se encontraron productos", url: "http://www.google.com"}];
+            }
+        });
+        */
+
+        return arr;
+    }
 });
