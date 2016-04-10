4e78e04e67cff2d2e77db4f901011760bb66f92e
SithMasterBDSM <jedilink@gmail.com>
2016-04-09 21:00:55 -0500

    0h30min: Colocando Meteor versión 1.2

--- runWithExternalMongo.sh
+++ runWithExternalMongo.sh
@@ -4,11 +4,11 @@ if [ $HOSTNAME == "test.ingenio-promocionales.com" ]; then
     echo RUNNING APPLICATION IN PRODUCTION MODE
     export ROOT_URL=http://test.ingenio-promocionales.com
     export MONGO_URL=mongodb://test.ingenio-promocionales.com:27017/ingenio
-    meteor --production --port 80 run
+    meteor --release 1.2.0.2 --production --port 80 run
 else
     echo RUNNING APPLICATION IN DEVELOPMENT MODE
     export ROOT_URL=http://localhost:3000
     export MONGO_URL=mongodb://localhost:27017/ingenio
-    meteor --port 3000 run
+    meteor --release 1.2.0.2 --port 3000 run
 fi
 
