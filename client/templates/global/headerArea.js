29bab4e023115ec7747c2a33f6a08887a0943c05
Gιαисαяlσ Pιeяιиι <Gl Pe>
2016-04-11 16:47:02 -0430

    Arreglo del modal de registro.

--- client/templates/global/headerArea.js
+++ client/templates/global/headerArea.js
@@ -4,8 +4,11 @@ Template.headerArea.events({
     da click en el enlace de crear un nuevo usuario registrado.
     */
     "click #registerNewUser": function (event, template) {
-        event.preventDefault();
-        $("#userRegistrationModalDialog").modal("show");
+        setTimeout(function () {
+            $(".modal-backdrop").removeClass("modal-backdrop");
+        }, 200);
+        /*event.preventDefault();
+        $("#userRegistrationModalDialog").modal("show");*/
     },
     /**
     Esta función se llama cuando un usuario registrado en sesión activa
